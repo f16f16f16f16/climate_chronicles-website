72f93536dba6284ea9b6eaf14a97f64fb5e6a4e3
Nontaphat Pongpis <momo5101@hotmail.com>
2024-02-07 19:30:36 +0700

    Fix AbtUs Page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -8,27 +8,27 @@ import ImgO from "../../assets/images/Owen.jpg"
 const Aboutus = () => (
   <div>
     <Navibar />
-    <p className="flex justify-center items-center p-6 text-8xl text-black">
+    <p className="flex justify-center items-center p-6 mt-8 text-black text-5xl md:text-8xl ">
       About Us
     </p>
-    <div class="flex flex-row justify-center space-x-8 p-10 h-screen">
+    <div class="flex flex-col items-center space-y-8 p-10 md:flex-row md:justify-center  md:space-y-0 md:space-x-8">
       <div class="flex flex-col items-center space-y-2">
-        <Image className="w-72 h-80 object-cover mb-2" src={ImgF} />
-        <span class="block font-semibold text-lg text-gray-900">Nontaphat Pongpis</span>
-        <span class="block text-sm text-gray-600">folkyuseforwork@gmail.com</span>
-        <span class="block text-sm text-blue-500">Programmer (Frontend, Backend)</span>
+        <Image className="w-48 md:w-72 h-60 md:h-80 object-cover mb-2" src={ImgF} />
+        <span class="block font-semibold text-md md:text-lg text-gray-900">Nontaphat Pongpis</span>
+        <span class="block text-xs md:text-sm text-gray-600">folkyuseforwork@gmail.com</span>
+        <span class="block text-xs md:text-sm text-blue-500">Programmer (Frontend, Backend)</span>
       </div>
       <div class="flex flex-col items-center space-y-2">
-        <Image className="w-72 h-80 object-cover mb-2" src={ImgJ} />
-        <span class="block font-semibold text-lg text-gray-900">Pomphet Damlert</span>
-        <span class="block text-sm text-gray-600">pomphetdamlert@gmail.com</span>
-        <span class="block text-sm text-blue-500">Programmer (Backend)</span>
+        <Image className="w-48 md:w-72 h-60 md:h-80 object-cover mb-2" src={ImgJ} />
+        <span class="block font-semibold text-md md:text-lg text-gray-900">Pomphet Damlert</span>
+        <span class="block text-xs md:text-sm text-gray-600">pomphetdamlert@gmail.com</span>
+        <span class="block text-xs md:text-sm text-blue-500">Programmer (Backend)</span>
       </div>
       <div class="flex flex-col items-center space-y-2">
-        <Image className="w-72 h-80 object-cover mb-2" src={ImgO} />
-        <span class="block font-semibold text-lg text-gray-900">Tanapat Kongnomnan</span>
-        <span class="block text-sm text-gray-600">owensickzz@gmail.com</span>
-        <span class="block text-sm text-blue-500">UX/UI Designer</span>
+        <Image className="w-48 md:w-72 h-60 md:h-80 object-cover mb-2" src={ImgO} />
+        <span class="block font-semibold text-md md:text-lg text-gray-900">Tanapat Kongnomnan</span>
+        <span class="block text-xs md:text-sm text-gray-600">owensickzz@gmail.com</span>
+        <span class="block text-xs md:text-sm text-blue-500">UX/UI Designer</span>
       </div>
     </div>
   </div>
