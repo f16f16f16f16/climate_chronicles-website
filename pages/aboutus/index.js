161c2da8e2efef32d2f2b22c7a202d6d91575e25
Nontaphat Pongpis <momo5101@hotmail.com>
2024-02-07 22:22:39 +0700

    Change image in AbtUs

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Navibar from "../../components/navbar/index";
-import ImgF from "../../assets/images/Folk.jpeg"
+import ImgF from "../../assets/images/Folk.png"
 import ImgJ from "../../assets/images/Joke.png"
-import ImgO from "../../assets/images/Owen.jpg"
+import ImgO from "../../assets/images/Owen.png"
 
 
 const Aboutus = () => (
@@ -13,22 +13,22 @@ const Aboutus = () => (
     </p>
     <div class="flex flex-col items-center space-y-8 p-10 md:flex-row md:justify-center  md:space-y-0 md:space-x-8">
       <div class="flex flex-col items-center space-y-2">
-        <Image className="w-48 md:w-72 h-60 md:h-80 object-cover mb-2" src={ImgF} />
-        <span class="block font-semibold text-md md:text-lg text-gray-900">Nontaphat Pongpis</span>
+        <Image className="w-60 md:w-72 " src={ImgF} />
+        {/* <span class="block font-semibold text-md md:text-lg text-gray-900">Nontaphat Pongpis</span>
         <span class="block text-xs md:text-sm text-gray-600">folkyuseforwork@gmail.com</span>
-        <span class="block text-xs md:text-sm text-blue-500">Programmer (Frontend, Backend)</span>
+        <span class="block text-xs md:text-sm text-blue-500">Programmer (Frontend, Backend)</span> */}
       </div>
       <div class="flex flex-col items-center space-y-2">
-        <Image className="w-48 md:w-72 h-60 md:h-80 object-cover mb-2" src={ImgJ} />
-        <span class="block font-semibold text-md md:text-lg text-gray-900">Pomphet Damlert</span>
+        <Image className="w-60 md:w-72 " src={ImgJ} />
+        {/* <span class="block font-semibold text-md md:text-lg text-gray-900">Pomphet Damlert</span>
         <span class="block text-xs md:text-sm text-gray-600">pomphetdamlert@gmail.com</span>
-        <span class="block text-xs md:text-sm text-blue-500">Programmer (Backend)</span>
+        <span class="block text-xs md:text-sm text-blue-500">Programmer (Backend)</span> */}
       </div>
       <div class="flex flex-col items-center space-y-2">
-        <Image className="w-48 md:w-72 h-60 md:h-80 object-cover mb-2" src={ImgO} />
-        <span class="block font-semibold text-md md:text-lg text-gray-900">Tanapat Kongnomnan</span>
+        <Image className="w-60 md:w-72 " src={ImgO} />
+        {/* <span class="block font-semibold text-md md:text-lg text-gray-900">Tanapat Kongnomnan</span>
         <span class="block text-xs md:text-sm text-gray-600">owensickzz@gmail.com</span>
-        <span class="block text-xs md:text-sm text-blue-500">UX/UI Designer</span>
+        <span class="block text-xs md:text-sm text-blue-500">UX/UI Designer</span> */}
       </div>
     </div>
   </div>
