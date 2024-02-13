aecaee340f56fff5d2f920b9871ccc87552a7c57
Nontaphat Pongpis <momo5101@hotmail.com>
2024-02-13 18:46:11 +0700

    Add logo and title of website

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,22 @@
+import Head from "next/head";
+import { NextUIProvider } from "@nextui-org/react";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+
 import "@/styles/globals.css";
-import 'mapbox-gl/dist/mapbox-gl.css';
-import {NextUIProvider} from '@nextui-org/react'
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <NextUIProvider className="bg-blue-200">
+      <Head>
+        <title>Climate Chronicles - Stories of Origin &amp; Impact in Our Environment</title>
+      </Head>
       <Component {...pageProps} />
       <Analytics />
       <SpeedInsights />
     </NextUIProvider>
-  )
+  );
 }
 
 export default MyApp;
