b1f5a215338be99c8cd79bd84488fa6940b0a490
Nontaphat Pongpis <momo5101@hotmail.com>
2024-02-13 14:39:16 +0700

    Add Vercel SpeedInsights

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import "@/styles/globals.css";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import {NextUIProvider} from '@nextui-org/react'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   return (
     <NextUIProvider className="bg-blue-200">
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </NextUIProvider>
   )
 }
