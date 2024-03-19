42982819f41a7a7ac896eb36039498a8e16d74ac
Nontaphat Pongpis <momo5101@hotmail.com>
2024-03-19 18:24:45 +0700

    Add  overflow-hidden

--- components/frame1/index.js
+++ components/frame1/index.js
@@ -10,7 +10,7 @@ import God from "../../assets/frame1/God.gif";
 
 const Frame1 = () => {
     return (
-      <div className="relative p-8 md:p-20 xl:p-20 flex justify-between items-center space-x-2 md:space-x-4 xl:space-x-4 bg-blue-100">
+      <div className="relative p-8 md:p-20 xl:p-20 flex justify-between items-center space-x-2 md:space-x-4 xl:space-x-4 bg-blue-100 overflow-hidden">
         {/* Background container for clouds, covering the entire component */}
         <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center space-x-1 md:space-x-2 xl:space-x-2 z-0">
           <Image src={Cloud1} className="w-1/2 md:w-1/2 xl:w-2/3 h-auto opacity-50" alt="Cloud 1" />
@@ -22,19 +22,14 @@ const Frame1 = () => {
         <div className="relative z-10 flex-shrink-0">
           <Image src={DekSpeaking} className="w-20 md:w-48 xl:w-60 h-auto" alt="Dek Speaking" />
         </div>
-        
-        {/* Spacer to evenly distribute space */}
-        <div className="flex-1"></div>
     
         {/* Heaven and God, with God positioned over Heaven */}
         <div className="relative z-10 flex-shrink-0">
-          <Image src={Heaven} className="w-48 md:w-[28rem] xl:w-[36rem] h-auto" alt="Heaven" />
+          <Image src={Heaven} className="w-48 md:w-[28rem] xl:w-[36rem] h-auto " alt="Heaven" />
           <Image src={God} className="w-24 md:w-64 xl:w-80 h-auto absolute top-4 md:top-8 xl:top-10 left-1/2 transform -translate-x-1/2" alt="God" />
         </div>
       </div>
     );
   };
   
-  
-
 export default Frame1;
