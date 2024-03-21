2d63f589bb3ae91cc11cfac0b794869f1a654d53
Nontaphat Pongpis <momo5101@hotmail.com>
2024-03-21 20:54:21 +0700

    Change structure project

--- components/frame1/index.js
+++ components/frame1/index.js
@@ -1,14 +1,39 @@
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import Image from "next/image";
 
-import Climate from "../../assets/frame1/Climate.png";
-import DekSpeaking from "../../assets/frame1/DekSpeaking.gif";
-import Cloud1 from "../../assets/frame1/Cloud1.gif";
-import Cloud2 from "../../assets/frame1/Cloud2.gif";
-import Cloud3 from "../../assets/frame1/Cloud3.gif";
-import Heaven from "../../assets/frame1/Heaven.gif";
-import God from "../../assets/frame1/God.gif";
+import Climate from "../../assets/frame1/images/Climate.png";
+import Dek from "../../assets/frame1/images/Dek.png";
+import Cloud1 from "../../assets/frame1/images/Cloud1.png";
+import Cloud2 from "../../assets/frame1/images/Cloud2.png";
+import Cloud3 from "../../assets/frame1/images/Cloud3.png";
+import Heaven from "../../assets/frame1/images/Heaven.png";
+import God from "../../assets/frame1/images/God.png";
 
 const Frame1 = () => {
+  const dekRef = useRef(null);
+
+  useEffect(() => {
+    // Register the ScrollTrigger plugin
+    gsap.registerPlugin(ScrollTrigger);
+
+    // GSAP animation with ScrollTrigger
+    gsap.to(dekRef.current, {
+      scrollTrigger: {
+        trigger: dekRef.current, // Use the Dek image as the trigger
+        start: "top center", // Start the animation when the top of Dek hits the center of the viewport
+        end: "bottom top", // End the animation when the bottom of Dek is at the top of the viewport
+        scrub: 1, // Smooth scrubbing, linking animation progress to scroll progress
+        markers: true, // Shows start and end markers; useful for debugging, remove in production
+      },
+      rotation: 360, // Rotate by 360 degrees
+      y: 200, // Move Dek 200 pixels down
+      ease: "none", // No easing for smooth, continuous animation
+    });
+  }, []);
+
   return (
     <div>
       <div className="flex justify-center items-center w-full p-24 bg-white">
@@ -16,72 +41,70 @@ const Frame1 = () => {
       </div>
 
       <div className="relative p-4 md:p-10 xl:p-20 py-16 md:py-28 xl:py-32 bg-blue-100 overflow-hidden flex flex-col items-center">
-        {/* Responsive background container for clouds */}
-        <div className="absolute inset-0 flex flex-wrap justify-around items-center space-x-1 space-y-1 md:space-x-2 md:space-y-2 xl:space-x-2 xl:space-y-2 z-0 opacity-50">
-          {/* Clouds background, responsive sizes */}
-          <Image
-            src={Cloud1}
-            className="w-1/3 h-auto"
-            alt="Cloud Background 1"
-          />
-          <Image
-            src={Cloud2}
-            className="w-1/3 h-auto"
-            alt="Cloud Background 2"
-          />
-          <Image
-            src={Cloud3}
-            className="w-1/3 h-auto"
-            alt="Cloud Background 3"
-          />
-          <Image
-            src={Cloud1}
-            className="w-1/4 h-auto"
-            alt="Cloud Background 4"
-          />
-          <Image
-            src={Cloud2}
-            className="w-1/4 h-auto"
-            alt="Cloud Background 5"
-          />
-        </div>
+        <div>
+          <div className="absolute inset-0 flex flex-wrap justify-around items-center z-0 opacity-50">
+            <Image
+              src={Cloud1}
+              className="w-1/3 h-auto"
+              alt="Cloud Background 1"
+            />
+            <Image
+              src={Cloud2}
+              className="w-1/3 h-auto"
+              alt="Cloud Background 2"
+            />
+            <Image
+              src={Cloud3}
+              className="w-1/3 h-auto"
+              alt="Cloud Background 3"
+            />
+            <Image
+              src={Cloud1}
+              className="w-1/4 h-auto"
+              alt="Cloud Background 4"
+            />
+            <Image
+              src={Cloud2}
+              className="w-1/4 h-auto"
+              alt="Cloud Background 5"
+            />
+          </div>
 
-        {/* Spotlight effect for Heaven and God */}
-        <div
-          className="relative z-20 mt-4 md:mt-8 xl:mt-16"
-          style={{
-            boxShadow: "0 0 100px 50px rgba(255, 215, 0, 0.5)", // Soft yellow glow
-            borderRadius: "50%", // Makes the shadow spread in a circular shape
-          }}
-        >
-          <div className="relative">
-            <Image
-              src={Heaven}
-              className="w-56 md:w-[24rem] xl:w-[36rem] h-auto"
-              alt="Heaven"
-            />
-            <Image
-              src={God}
-              className="w-28 md:w-56 xl:w-72 h-auto absolute -top-6 md:-top-12 xl:-top-20 left-1/2 transform -translate-x-1/2"
-              alt="God"
-            />
+          <div
+            className="relative z-20 mt-4 md:mt-8 xl:mt-16"
+            style={{
+              boxShadow: "0 0 100px 50px rgba(255, 215, 0, 0.5)", // Soft yellow glow
+              borderRadius: "50%", // Makes the shadow spread in a circular shape
+            }}
+          >
+            <div className="relative">
+              <Image
+                src={Heaven}
+                className="w-96 md:w-[60rem] xl:w-[80rem] h-auto"
+                alt="Heaven"
+              />
+              <Image
+                src={God}
+                className="w-36 md:w-96 xl:w-[30rem] h-auto absolute top-16 md:top-32 xl:top-52 left-1/2 transform -translate-x-1/2"
+                alt="God"
+              />
+            </div>
           </div>
         </div>
 
         {/* Responsive spacer */}
         <div className="mt-14 md:mt-24 xl:mt-36"></div>
 
-        {/* DekSpeaking Image, responsive */}
-        <div className="relative z-30">
+        <div className="relative z-30 -bottom-20 md:-bottom-40 xl:-bottom-60">
           <Image
-            src={DekSpeaking}
-            className="w-16 md:w-36 xl:w-48 h-auto"
+            src={Dek}
+            className="w-20 md:w-44 xl:w-64 h-auto"
             alt="Dek Speaking"
+            ref={dekRef} // Step 2: Apply the reference to the Image component
           />
         </div>
 
-        {/* Expanded and responsive foreground cloud layer */}
-        <div className="absolute bottom-0 left-0 right-0 z-10 h-32 md:h-48 xl:h-64 overflow-hidden">
+        <div className="relative z-10 -bottom-10 md:-bottom-20 xl:-bottom-30 h-20 md:h-48 xl:h-64 overflow-hidden">
           <Image
             src={Cloud1}
             className="w-full h-full opacity-70"
