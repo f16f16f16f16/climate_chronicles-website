5e5af8ceeb897093b19c91af3f20cb0ae373952b
Nontaphat Pongpis <momo5101@hotmail.com>
2024-04-03 01:52:32 +0700

    Change animation of gas (Frame2)

--- components/frame2/index.js
+++ components/frame2/index.js
@@ -8,25 +8,33 @@ import { MotionPathPlugin } from "gsap/dist/MotionPathPlugin";
 import CCMan from "../../assets/frame2/gif/CCManFly.gif";
 import Aura from "../../assets/frame2/images/Aura.png";
 import UFO from "../../assets/frame2/images/UFO.png";
-import CH4 from "../../assets/frame2/images/CH4.png";
-import CO2 from "../../assets/frame2/images/CO2.png";
-import HFCs from "../../assets/frame2/images/HFCs.png";
-import N20 from "../../assets/frame2/images/N2O.png";
-import PFCs from "../../assets/frame2/images/PFCs.png";
-import SF6 from "../../assets/frame2/images/SF6.png";
+import CH4_1 from "../../assets/frame2/images/gasSeparate/CH4.1.png";
+import CH4_2 from "../../assets/frame2/images/gasSeparate/CH4.2.png";
+import CO2_1 from "../../assets/frame2/images/gasSeparate/CO2.1.png";
+import CO2_2 from "../../assets/frame2/images/gasSeparate/CO2.2.png";
+import HFCs_1 from "../../assets/frame2/images/gasSeparate/HFCs.1.png";
+import HFCs_2 from "../../assets/frame2/images/gasSeparate/HFCs.2.png";
+import N2O_1 from "../../assets/frame2/images/gasSeparate/N2O.1.png";
+import N2O_2 from "../../assets/frame2/images/gasSeparate/N2O.2.png";
+import PFCs_1 from "../../assets/frame2/images/gasSeparate/PFCs.1.png";
+import PFCs_2 from "../../assets/frame2/images/gasSeparate/PFCs.2.png";
+import SF6_1 from "../../assets/frame2/images/gasSeparate/SF6.1.png";
+import SF6_2 from "../../assets/frame2/images/gasSeparate/SF6.2.png";
 
 // Register GSAP plugins outside of the component to avoid registering multiple times.
 gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
 
 const Frame2 = () => {
   const ccmanRef = useRef(null);
-  // Handler to add the animation class to the clicked image
-  const handleImageClick = (event) => {
-    // Ensure only the img element inside the button triggers the animation
-    const imgElement = event.currentTarget.querySelector("img");
-    if (imgElement) {
-      imgElement.className = "rollingImage";
-    }
+  // Initialize state as an object where keys are image set identifiers and values are booleans
+  const [isActive, setIsActive] = useState({ CH4: false, CO2: false, HFCs: false, N2O: false, PFCs: false, SF6: false });
+
+  // Handler function that takes an image set identifier
+  const handleImageClick = (id) => {
+    setIsActive(prevState => ({
+      ...prevState,
+      [id]: !prevState[id] // Toggle the state for the specific image set
+    }));
   };
 
   useEffect(() => {
@@ -105,25 +113,45 @@ const Frame2 = () => {
             src={Aura}
             className="h-72 md:h-[35rem] xl:h-[64rem] 2xl:h-[85rem] 2xl:w-screen object-cover"
           />
-          <div className="w-44 md:w-96 xl:w-[40rem] space-y-20 md:space-y-32 xl:space-y-52">
-            <button onClick={handleImageClick}>
-              <Image src={CH4} alt="CH4" />
-            </button>
-            <button onClick={handleImageClick}>
-              <Image src={CO2} alt="CO2" />
-            </button>
-            <button onClick={handleImageClick}>
-              <Image src={HFCs} alt="HFCs" />
-            </button>
-            <button onClick={handleImageClick}>
-              <Image src={N20} alt="N2O" />
-            </button>
-            <button onClick={handleImageClick}>
-              <Image src={PFCs} alt="PFCs" />
-            </button>
-            <button onClick={handleImageClick}>
-              <Image src={SF6} alt="SF6" />
-            </button>
+          <div className="w-44 md:w-96 xl:w-[40rem]">
+            <div className="w-1/2 md:w-full space-y-[100%]">
+              <div>
+                <button className="flex -space-x-[3%]" onClick={() => handleImageClick('CH4')}>
+                  <Image src={CH4_1} alt="CH4" className={`transition-transform duration-500 ${isActive.CH4 ? '-translate-x-[100%]' : ''}`}/>
+                  <Image src={CH4_2} alt="CH4" className={`transition-transform duration-500 ${isActive.CH4 ? 'translate-x-[100%]' : ''}`}/>
+                </button>
+              </div>
+              <div>
+                <button className="flex -space-x-[3%]" onClick={() => handleImageClick('CO2')}>
+                  <Image src={CO2_1} alt="CO2" className={`transition-transform duration-500 ${isActive.CO2 ? '-translate-x-[100%]' : ''}`}/>
+                  <Image src={CO2_2} alt="CO2" className={`transition-transform duration-500 ${isActive.CO2 ? 'translate-x-[100%]' : ''}`}/>
+                </button>
+              </div>
+              <div>
+                <button className="flex -space-x-[3%]" onClick={() => handleImageClick('HFCs')}>
+                  <Image src={HFCs_1} alt="HFCs" className={`transition-transform duration-500 ${isActive.HFCs ? '-translate-x-[100%]' : ''}`}/>
+                  <Image src={HFCs_2} alt="HFCs" className={`transition-transform duration-500 ${isActive.HFCs ? 'translate-x-[100%]' : ''}`}/>
+                </button>
+              </div>
+              <div>
+                <button className="flex -space-x-[3%]" onClick={() => handleImageClick('N2O')}>
+                  <Image src={N2O_1} alt="N2O" className={`transition-transform duration-500 ${isActive.N2O ? '-translate-x-[100%]' : ''}`}/>
+                  <Image src={N2O_2} alt="N2O" className={`transition-transform duration-500 ${isActive.N2O ? 'translate-x-[100%]' : ''}`}/>
+                </button>
+              </div>
+              <div>
+                <button className="flex -space-x-[3%]" onClick={() => handleImageClick('PFCs')}>
+                  <Image src={PFCs_1} alt="PFCs" className={`transition-transform duration-500 ${isActive.PFCs ? '-translate-x-[100%]' : ''}`}/>
+                  <Image src={PFCs_2} alt="PFCs" className={`transition-transform duration-500 ${isActive.PFCs ? 'translate-x-[100%]' : ''}`}/>
+                </button>
+              </div>
+              <div>
+                <button className="flex -space-x-[3%]" onClick={() => handleImageClick('SF6')}>
+                  <Image src={SF6_1} alt="SF6" className={`transition-transform duration-500 ${isActive.SF6 ? '-translate-x-[100%]' : ''}`}/>
+                  <Image src={SF6_2} alt="SF6" className={`transition-transform duration-500 ${isActive.SF6 ? 'translate-x-[100%]' : ''}`}/>
+                </button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
