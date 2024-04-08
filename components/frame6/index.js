eaae79487ce53a6f2dd0f43a78f0799398e269ac
Nontaphat Pongpis <momo5101@hotmail.com>
2024-04-09 01:57:50 +0700

    Add GasFloat

--- components/frame6/index.js
+++ components/frame6/index.js
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 import BigFrame from "../../assets/frame6/gif/BigFrame.gif";
 import Boomer from "../../assets/frame6/gif/Boomer.gif";
+import GasFloat from "../gasFloat";
 
 const Frame6 = () => {
   return (
     <div className="mt-96 overflow-hidden">
       <div className="bg-sky-200">
-      <Image src={BigFrame} alt="BigFrame"/>
-      <Image src={Boomer} alt="Boomer" className="w-1/3 -mt-[24%] ml-[60%]"/>
+        <Image src={BigFrame} alt="BigFrame" />
+        <Image src={Boomer} alt="Boomer" className="w-1/3 -mt-[24%] ml-[60%]" />
+        <GasFloat />
       </div>
     </div>
   );
