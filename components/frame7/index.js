bd903a89b010bb0f60d7ba0c7204ff0af58fc3e8
Nontaphat Pongpis <momo5101@hotmail.com>
2024-04-11 17:19:53 +0700

    Add btn to path  /quiz

--- components/frame7/index.js
+++ components/frame7/index.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button, Link } from "@nextui-org/react";
 
 import BigFrame from "../../assets/frame7/gif/Wastewater.gif";
 
@@ -7,6 +8,13 @@ const Frame7 = () => {
     <div className="overflow-hidden">
       <div className="bg-green-300">
         <Image src={BigFrame} alt="BigFrame" />
+      </div>
+      <div className="bg-rose-950 py-24 flex justify-center">
+        <Link href="/quiz">
+          <Button color="secondary" variant="faded">
+            Go Answer Questions!!!
+          </Button>
+        </Link>
       </div>
     </div>
   );
