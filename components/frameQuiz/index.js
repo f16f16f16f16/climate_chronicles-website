367868bb025b02ddd80924bd5b3343eb9b1ea6f2
Nontaphat Pongpis <momo5101@hotmail.com>
2024-04-13 03:17:51 +0700

    Add FrameQuiz

--- components/frameQuiz/index.js
+++ components/frameQuiz/index.js
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
+import CCMan from "../../assets/frame1/gif/CCMan_Stand&speak.gif";
+import God from "../../assets/frameQuiz/images/GOD.png";
 
 const FrameQuiz = () => {
   return (
-    <div>
-
+    <div className="flex flex-row justify-center items-center py-20 space-x-[40%]">
+      <div className="w-[30%]">
+        <Image src={CCMan} alt="CCMan" />
+      </div>
+      <div className="w-[20%]">
+        <Image src={God} alt="God" />
+      </div>
     </div>
   );
 };
