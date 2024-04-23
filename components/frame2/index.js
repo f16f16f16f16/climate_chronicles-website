138950a8ae0b8f928addaa17e7d2d44f7f520286
Nontaphat Pongpis <momo5101@hotmail.com>
2024-04-23 22:21:01 +0700

    Update Gas (Frame2)

--- components/frame2/index.js
+++ components/frame2/index.js
@@ -113,203 +113,203 @@ const Frame2 = () => {
   }, []);
 
   return (
-      <div className="bg-gradient-to-b from-zinc-900 via-indigo-700 to-sky-200 py-28 md:py-32 xl:py-40 overflow-hidden">
-        <div className="ufo">
-          <Image className="w-[50%]" src={UFO} alt="ufo" />
-        </div>
-        <div ref={ccmanRef}>
-          <Image
-            src={CCMan}
-            alt="CCMan Flying"
-            className="ccman-image w-52 md:w-96 xl:w-[35rem]"
-          />
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-white text-4xl md:text-6xl lg:text-8xl py-5 font-bold shadow-md">
-            Ozone Layer
-          </h1>
-          <Image
-            src={Aura}
-            className="h-72 md:h-[45rem] xl:h-[64rem] 2xl:h-[85rem] 2xl:w-screen object-cover"
-          />
-          <div className="space-y-48 md:space-y-[34rem] xl:space-y-[60rem]">
-            <div className="flex flex-col items-center justify-center">
-              <Textarea
-                isReadOnly
-                variant="bordered"
-                className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
-                  zIndexes.CH4 === 10 ? "z-10" : "z-0"
+    <div className="bg-gradient-to-b from-zinc-900 via-indigo-700 to-sky-200 py-28 md:py-32 xl:py-40 overflow-hidden">
+      <div className="ufo">
+        <Image className="w-[50%]" src={UFO} alt="ufo" />
+      </div>
+      <div ref={ccmanRef}>
+        <Image
+          src={CCMan}
+          alt="CCMan Flying"
+          className="ccman-image w-52 md:w-96 xl:w-[35rem]"
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-white text-4xl md:text-6xl lg:text-8xl py-5 font-bold shadow-md">
+          Ozone Layer
+        </h1>
+        <Image
+          src={Aura}
+          className="h-72 md:h-[45rem] xl:h-[64rem] 2xl:h-[85rem] 2xl:w-screen object-cover"
+        />
+        <div className="space-y-48 md:space-y-[34rem] xl:space-y-[60rem]">
+          <div className="flex flex-col items-center justify-center">
+            <Textarea
+              isReadOnly
+              variant="bordered"
+              className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
+                zIndexes.CH4 === 10 ? "z-10" : "z-0"
+              }`}
+              defaultValue="This is methane gas (CH4), a greenhouse gas released into the Earth's atmosphere in large quantities. It occurs naturally but 60% of it in the atmosphere is due to human activities, which can have a greater greenhouse effect than carbon dioxide, up to 25 times more."
+            />
+            <button
+              className="absolute flex -space-x-[1%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
+              onClick={() => handleImageClick("CH4")}
+            >
+              <Image
+                src={CH4_1}
+                alt="CH4"
+                className={`transition-transform duration-500 ${
+                  isActive.CH4 ? "-translate-x-[100%]" : ""
                 }`}
-                defaultValue="This is methane gas (CH4), a greenhouse gas released into the Earth's atmosphere in large quantities. It occurs naturally but 60% of it in the atmosphere is due to human activities, which can have a greater greenhouse effect than carbon dioxide, up to 25 times more."
               />
-              <button
-                className="absolute flex -space-x-[3%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
-                onClick={() => handleImageClick("CH4")}
-              >
-                <Image
-                  src={CH4_1}
-                  alt="CH4"
-                  className={`transition-transform duration-500 ${
-                    isActive.CH4 ? "-translate-x-[100%]" : ""
-                  }`}
-                />
-                <Image
-                  src={CH4_2}
-                  alt="CH4"
-                  className={`transition-transform duration-500 ${
-                    isActive.CH4 ? "translate-x-[120%]" : ""
-                  }`}
-                />
-              </button>
-            </div>
-            <div className="flex flex-col items-center justify-center">
-              <Textarea
-                isReadOnly
-                variant="bordered"
-                className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
-                  zIndexes.CO2 === 10 ? "z-10" : "z-0"
+              <Image
+                src={CH4_2}
+                alt="CH4"
+                className={`transition-transform duration-500 ${
+                  isActive.CH4 ? "translate-x-[120%]" : ""
                 }`}
-                defaultValue="This is CO2 (Carbon dioxide), a greenhouse gas released into the atmosphere, having the most significant influence on trapping heat in the atmosphere. It can occur naturally, but humans have become the main contributors to its production and release. It's mainly from activities like burning fossil fuels, especially coal, oil, and natural gas, which rapidly increase the amount of carbon dioxide."
               />
-              <button
-                className="absolute flex -space-x-[3%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
-                onClick={() => handleImageClick("CO2")}
-              >
-                <Image
-                  src={CO2_1}
-                  alt="CO2"
-                  className={`transition-transform duration-500 ${
-                    isActive.CO2 ? "-translate-x-[100%]" : ""
-                  }`}
-                />
-                <Image
-                  src={CO2_2}
-                  alt="CO2"
-                  className={`transition-transform duration-500 ${
-                    isActive.CO2 ? "translate-x-[120%]" : ""
-                  }`}
-                />
-              </button>
-            </div>
-            <div className="flex flex-col items-center justify-center">
-              <Textarea
-                isReadOnly
-                variant="bordered"
-                className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
-                  zIndexes.HFCs === 10 ? "z-10" : "z-0"
+            </button>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <Textarea
+              isReadOnly
+              variant="bordered"
+              className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
+                zIndexes.CO2 === 10 ? "z-10" : "z-0"
+              }`}
+              defaultValue="This is CO2 (Carbon dioxide), a greenhouse gas released into the atmosphere, having the most significant influence on trapping heat in the atmosphere. It can occur naturally, but humans have become the main contributors to its production and release. It's mainly from activities like burning fossil fuels, especially coal, oil, and natural gas, which rapidly increase the amount of carbon dioxide."
+            />
+            <button
+              className="absolute flex -space-x-[1%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
+              onClick={() => handleImageClick("CO2")}
+            >
+              <Image
+                src={CO2_1}
+                alt="CO2"
+                className={`transition-transform duration-500 ${
+                  isActive.CO2 ? "-translate-x-[100%]" : ""
                 }`}
-                defaultValue="HFCs (Hydrofluorocarbons) are synthetic greenhouse gases used in various cooling systems such as air conditioners, refrigerators, sprays, and fire extinguishers. They have a high heat-trapping potential and can contribute significantly to the greenhouse effect, even more so than carbon dioxide, ranging from 124 to 14,800 times."
               />
-              <button
-                className="absolute flex -space-x-[3%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
-                onClick={() => handleImageClick("HFCs")}
-              >
-                <Image
-                  src={HFCs_1}
-                  alt="HFCs"
-                  className={`transition-transform duration-500 ${
-                    isActive.HFCs ? "-translate-x-[100%]" : ""
-                  }`}
-                />
-                <Image
-                  src={HFCs_2}
-                  alt="HFCs"
-                  className={`transition-transform duration-500 ${
-                    isActive.HFCs ? "translate-x-[120%]" : ""
-                  }`}
-                />
-              </button>
-            </div>
-            <div className="flex flex-col items-center justify-center">
-              <Textarea
-                isReadOnly
-                variant="bordered"
-                className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
-                  zIndexes.N2O === 10 ? "z-10" : "z-0"
+              <Image
+                src={CO2_2}
+                alt="CO2"
+                className={`transition-transform duration-500 ${
+                  isActive.CO2 ? "translate-x-[120%]" : ""
                 }`}
-                defaultValue="N2O (Nitrous oxide), Another important greenhouse gas affecting the Earth's climate. It's naturally occurring, like from lightning, volcanic eruptions, and the decay of organic matter. However, it's also produced by human activities such as agricultural practices like using chemical fertilizers, burning fossil fuels, and livestock farming, which accelerate its release into the atmosphere. This can have long-term environmental impacts and contribute to increased heat in the atmosphere."
               />
-              <button
-                className="absolute flex -space-x-[3%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
-                onClick={() => handleImageClick("N2O")}
-              >
-                <Image
-                  src={N2O_1}
-                  alt="N2O"
-                  className={`transition-transform duration-500 ${
-                    isActive.N2O ? "-translate-x-[100%]" : ""
-                  }`}
-                />
-                <Image
-                  src={N2O_2}
-                  alt="N2O"
-                  className={`transition-transform duration-500 ${
-                    isActive.N2O ? "translate-x-[120%]" : ""
-                  }`}
-                />
-              </button>
-            </div>
-            <div className="flex flex-col items-center justify-center">
-              <Textarea
-                isReadOnly
-                variant="bordered"
-                className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
-                  zIndexes.PFCs === 10 ? "z-10" : "z-0"
+            </button>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <Textarea
+              isReadOnly
+              variant="bordered"
+              className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
+                zIndexes.HFCs === 10 ? "z-10" : "z-0"
+              }`}
+              defaultValue="HFCs (Hydrofluorocarbons) are synthetic greenhouse gases used in various cooling systems such as air conditioners, refrigerators, sprays, and fire extinguishers. They have a high heat-trapping potential and can contribute significantly to the greenhouse effect, even more so than carbon dioxide, ranging from 124 to 14,800 times."
+            />
+            <button
+              className="absolute flex -space-x-[1%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
+              onClick={() => handleImageClick("HFCs")}
+            >
+              <Image
+                src={HFCs_1}
+                alt="HFCs"
+                className={`transition-transform duration-500 ${
+                  isActive.HFCs ? "-translate-x-[100%]" : ""
                 }`}
-                defaultValue="PFCs (Perfluorocarbons), synthetic greenhouse gases originating from industrial processes, primarily used as solvents and starting materials in various industrial productions. They have a high heat-trapping potential and can contribute significantly to the greenhouse effect, even more so than carbon dioxide."
               />
-              <button
-                className="absolute flex -space-x-[3%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
-                onClick={() => handleImageClick("PFCs")}
-              >
-                <Image
-                  src={PFCs_1}
-                  alt="PFCs"
-                  className={`transition-transform duration-500 ${
-                    isActive.PFCs ? "-translate-x-[100%]" : ""
-                  }`}
-                />
-                <Image
-                  src={PFCs_2}
-                  alt="PFCs"
-                  className={`transition-transform duration-500 ${
-                    isActive.PFCs ? "translate-x-[120%]" : ""
-                  }`}
-                />
-              </button>
-            </div>
-            <div className="flex flex-col items-center justify-center">
-              <Textarea
-                isReadOnly
-                variant="bordered"
-                className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
-                  zIndexes.SF6 === 10 ? "z-10" : "z-0"
+              <Image
+                src={HFCs_2}
+                alt="HFCs"
+                className={`transition-transform duration-500 ${
+                  isActive.HFCs ? "translate-x-[120%]" : ""
                 }`}
-                defaultValue="SF6 (Sulfur hexafluoride), a gas with high density and mass compared to air. It's commonly used in the electronics industry as electrical insulation to prevent electrical sparks from high-voltage electrical switchgear or to assist in dissipating heat from high-voltage electrical equipment."
               />
-              <button
-                className="absolute flex -space-x-[3%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
-                onClick={() => handleImageClick("SF6")}
-              >
-                <Image
-                  src={SF6_1}
-                  alt="SF6"
-                  className={`transition-transform duration-500 ${
-                    isActive.SF6 ? "-translate-x-[100%]" : ""
-                  }`}
-                />
-                <Image
-                  src={SF6_2}
-                  alt="SF6"
-                  className={`transition-transform duration-500 ${
-                    isActive.SF6 ? "translate-x-[120%]" : ""
-                  }`}
-                />
-              </button>
-            </div>
+            </button>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <Textarea
+              isReadOnly
+              variant="bordered"
+              className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
+                zIndexes.N2O === 10 ? "z-10" : "z-0"
+              }`}
+              defaultValue="N2O (Nitrous oxide), Another important greenhouse gas affecting the Earth's climate. It's naturally occurring, like from lightning, volcanic eruptions, and the decay of organic matter. However, it's also produced by human activities such as agricultural practices like using chemical fertilizers, burning fossil fuels, and livestock farming, which accelerate its release into the atmosphere. This can have long-term environmental impacts and contribute to increased heat in the atmosphere."
+            />
+            <button
+              className="absolute flex -space-x-[1%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
+              onClick={() => handleImageClick("N2O")}
+            >
+              <Image
+                src={N2O_1}
+                alt="N2O"
+                className={`transition-transform duration-500 ${
+                  isActive.N2O ? "-translate-x-[100%]" : ""
+                }`}
+              />
+              <Image
+                src={N2O_2}
+                alt="N2O"
+                className={`transition-transform duration-500 ${
+                  isActive.N2O ? "translate-x-[120%]" : ""
+                }`}
+              />
+            </button>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <Textarea
+              isReadOnly
+              variant="bordered"
+              className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
+                zIndexes.PFCs === 10 ? "z-10" : "z-0"
+              }`}
+              defaultValue="PFCs (Perfluorocarbons), synthetic greenhouse gases originating from industrial processes, primarily used as solvents and starting materials in various industrial productions. They have a high heat-trapping potential and can contribute significantly to the greenhouse effect, even more so than carbon dioxide."
+            />
+            <button
+              className="absolute flex -space-x-[1%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
+              onClick={() => handleImageClick("PFCs")}
+            >
+              <Image
+                src={PFCs_1}
+                alt="PFCs"
+                className={`transition-transform duration-500 ${
+                  isActive.PFCs ? "-translate-x-[100%]" : ""
+                }`}
+              />
+              <Image
+                src={PFCs_2}
+                alt="PFCs"
+                className={`transition-transform duration-500 ${
+                  isActive.PFCs ? "translate-x-[120%]" : ""
+                }`}
+              />
+            </button>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <Textarea
+              isReadOnly
+              variant="bordered"
+              className={`h-full w-[80%] md:w-[150%] xl:w-[200%] text-white ${
+                zIndexes.SF6 === 10 ? "z-10" : "z-0"
+              }`}
+              defaultValue="SF6 (Sulfur hexafluoride), a gas with high density and mass compared to air. It's commonly used in the electronics industry as electrical insulation to prevent electrical sparks from high-voltage electrical switchgear or to assist in dissipating heat from high-voltage electrical equipment."
+            />
+            <button
+              className="absolute flex -space-x-[1%] scale-[.65] md:scale-50 xl:scale-100 overflow-hidden"
+              onClick={() => handleImageClick("SF6")}
+            >
+              <Image
+                src={SF6_1}
+                alt="SF6"
+                className={`transition-transform duration-500 ${
+                  isActive.SF6 ? "-translate-x-[100%]" : ""
+                }`}
+              />
+              <Image
+                src={SF6_2}
+                alt="SF6"
+                className={`transition-transform duration-500 ${
+                  isActive.SF6 ? "translate-x-[120%]" : ""
+                }`}
+              />
+            </button>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
