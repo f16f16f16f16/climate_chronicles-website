ba29ef95b9f85fff539b5d6d520427210b8fc792
Nontaphat Pongpis <momo5101@hotmail.com>
2024-05-01 22:22:54 +0700

    Update Size of Frame5

--- components/frame5/index.js
+++ components/frame5/index.js
@@ -41,7 +41,7 @@ const Frame5 = () => {
 
   return (
     <div className="overflow-hidden">
-      <div className="bg-yellow-200 py-[20%]">
+      <div className="bg-yellow-200 py-[30%]">
         <div className="flex">
           <Image src={CCMan} className="w-[20%] ccman-animation3" />
           <Textarea
@@ -59,71 +59,71 @@ const Frame5 = () => {
           <Image
             src={Mill}
             alt="Mill"
-            className="w-1/3 -mt-[6rem] md:-mt-[12rem] xl:-mt-[22rem] 2xl:-mt-[28rem]"
+            className="w-[28%] -mt-[5rem] md:-mt-[11rem] xl:-mt-[20rem] 2xl:-mt-[24rem]"
           />
-          <Image src={Straw} alt="Straw" className="w-1/6" />
+          <Image src={Straw} alt="Straw" className="w-[12%]" />
           <Image
             src={Straw}
             alt="Straw"
-            className="w-1/6 -mt-10 md:-mt-20 xl:-mt-36 2xl:-mt-40 -ml-10 md:-ml-20 xl:-ml-32 2xl:-ml-40"
+            className="w-[12%] -mt-10 md:-mt-20 xl:-mt-36 2xl:-mt-40 -ml-10 md:-ml-20 xl:-ml-32 2xl:-ml-40"
           />
           <Image
             src={Straw}
             alt="Straw"
-            className="w-1/6 -ml-12 md:-ml-24 xl:-ml-36 2xl:-ml-52"
+            className="w-[12%] -ml-12 md:-ml-24 xl:-ml-36 2xl:-ml-52"
           />
         </div>
         <div className="flex items-center  ">
           <Image
             src={Fence}
             alt="Fence"
-            className="w-1/2 -mt-[5.4rem] md:-mt-[11rem] xl:-mt-[20rem] 2xl:-mt-[26rem]"
+            className="w-[50%] -mt-[5.4rem] md:-mt-[11rem] xl:-mt-[20rem] 2xl:-mt-[26rem]"
           />
           <Image
             src={Fertilizer}
             alt="Fertilizer"
-            className="w-1/12 -mt-6 md:-mt-16 xl:-mt-32 2xl:-mt-40 -ml-16 md:-ml-28 xl:-ml-56 2xl:-ml-72"
+            className="w-[7%] -mt-6 md:-mt-16 xl:-mt-32 2xl:-mt-40 -ml-16 md:-ml-28 xl:-ml-56 2xl:-ml-72"
           />
           <Image
             src={Fertilizer}
             alt="Fertilizer"
-            className="w-1/12 -mt-6 md:-mt-16 xl:-mt-32 2xl:-mt-40"
+            className="w-[7%] -mt-6 md:-mt-16 xl:-mt-32 2xl:-mt-40"
           />
         </div>
-        <div className="flex items-center gap-x-4 ">
+        <div className="flex items-center gap-x-10 md:gap-x-14 xl:gap-x-24 2xl:gap-x-32 ">
           <Image
             src={Farmer2}
             alt="Farmer"
-            className="w-1/6 -mt-20 md:-mt-32 xl:-mt-56 2xl:-mt-80 ml-4 md:ml-6 xl:ml-10 2xl:ml-12"
+            className="w-[10%] -mt-20 md:-mt-32 xl:-mt-56 2xl:-mt-80 ml-4 md:ml-6 xl:ml-10 2xl:ml-12"
           />
           <Image
             src={Cow}
             alt="Cow"
-            className="w-1/5 -mt-20 md:-mt-32 xl:-mt-56 2xl:-mt-80"
+            className="w-[15%] -mt-20 md:-mt-32 xl:-mt-56 2xl:-mt-80"
           />
           <Image
             src={Horse}
             alt="Horse"
-            className="w-1/5 -ml-28 md:-ml-52 xl:-ml-96 2xl:-ml-[30rem] -mb-12 md:-mb-32 xl:-mb-56 2xl:-mb-80"
+            className="w-[15%] -ml-28 md:-ml-52 xl:-ml-96 2xl:-ml-[30rem] md:-mb-12 xl:-mb-24 2xl:-mb-32"
           />
           <Image
             src={Farmer1}
             alt="Farmer"
-            className="w-[11%] -mt-[5rem] md:-mt-[7rem] xl:-mt-[12rem] 2xl:-mt-[18rem] ml-[2rem] md:ml-[7rem] xl:ml-[12rem] 2xl:ml-[18rem]"
+            className="w-[8%] -mt-[8rem] md:-mt-[9rem] xl:-mt-[18rem] 2xl:-mt-[26rem] ml-[2rem] md:ml-[7rem] xl:ml-[12rem] 2xl:ml-[18rem]"
           />
           <Image
             src={Tractor}
             alt="Tractor"
-            className="w-1/3 -mt-[12rem] md:-mt-[26rem] xl:-mt-[46rem] 2xl:-mt-[67rem]"
+            className="w-[25%] -mt-[12rem] md:-mt-[20rem] xl:-mt-[42rem] 2xl:-mt-[50rem]"
           />
           <Image
             src={Rice}
             alt="Rice"
-            className="w-5/12 -ml-48 md:-ml-[23rem] xl:-ml-[37rem] 2xl:-ml-[50rem] -mb-[9rem] md:-mb-[20rem] xl:-mb-[38rem] 2xl:-mb-[42rem]"
+            className="w-[35%] -ml-48 md:-ml-[23rem] xl:-ml-[37rem] 2xl:-ml-[50rem] -mb-[3rem] md:-mb-[12rem] xl:-mb-[20rem] 2xl:-mb-[24rem]"
           />
         </div>
         <div>
-          <Image src={Fence} alt="Fence" className="w-1/2 " />
+          <Image src={Fence} alt="Fence" className="w-1/2 -mt-20 md:-mt-28 xl:-mt-52 2xl:-mt-72" />
         </div>
       </div>
       <Textarea
