25f2f9009c6e97c0eca6106bf518945694e57e8d
Nontaphat Pongpis <momo5101@hotmail.com>
2024-05-04 15:44:05 +0700

    Update audio textArea (Frame3)

--- components/frame3/index.js
+++ components/frame3/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Image from "next/image";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
@@ -9,6 +9,7 @@ import {
   PopoverContent,
   Button,
 } from "@nextui-org/react";
+import { SpeakerWaveIcon } from "@heroicons/react/24/solid";
 
 import CCMan from "../../assets/frame1/gif/CCMan_Stand&speak.webp";
 import Plane from "../../assets/frame3/images/Plane.webp";
@@ -21,6 +22,20 @@ import Car2 from "../../assets/frame3/gif/Car2.webp";
 import Car3 from "../../assets/frame3/gif/Car3.webp";
 
 const Frame3 = () => {
+  const audioRef = useRef(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      audioRef.current = new Audio("/PCIII-Remembering-Past-Everything.mp3");
+    }
+  }, []);
+
+  const playAudio = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
   const content = (
     <PopoverContent>
       <div className="px-1 py-2">
@@ -65,24 +80,45 @@ const Frame3 = () => {
   return (
     <div className="bg-neutral-500">
       <div className="flex">
-        <Image src={CCMan} className="w-[20%] ccman-animation" />
-        <div>
-          <Textarea
-            isReadOnly
-            disableAutosize
-            className=""
-            color="primary"
-            label="CCMan"
-            defaultValue="Does everyone understand, right?"
-          />
-          <Textarea
-            isReadOnly
-            disableAutosize
-            className="absolute w-[60%]"
-            color="primary"
-            label="CCMan"
-            defaultValue="Next, Ccman will tell everyone about human activities that produce various gases, which also contribute to greenhouse gases. They also have the effect of increasing heat in the atmosphere"
-          />
+        <Image src={CCMan} className="absolute w-[20%] ccman-animation" />
+        <div className="ml-[20%] md:ml-[30%] xl:ml-[30%]">
+          <div className="flex">
+            <Button
+              isIconOnly
+              color="primary"
+              aria-label="Audio"
+              className=""
+              onClick={playAudio}
+            >
+              <SpeakerWaveIcon className="h-5 w-5" />
+            </Button>
+            <Textarea
+              isReadOnly
+              disableAutosize
+              className=""
+              color="primary"
+              label="CCMan"
+              defaultValue="Does everyone understand, right?"
+            />
+          </div>
+          <div className="flex mt-12">
+            <Button
+              isIconOnly
+              color="primary"
+              aria-label="Audio"
+              className=""
+              onClick={playAudio}
+            >
+              <SpeakerWaveIcon className="h-5 w-5" />
+            </Button>
+            <Textarea
+              isReadOnly
+              className=""
+              color="primary"
+              label="CCMan"
+              defaultValue="Next, Ccman will tell everyone about human activities that produce various gases, which also contribute to greenhouse gases. They also have the effect of increasing heat in the atmosphere"
+            />
+          </div>
         </div>
       </div>
       <div className="flex justify-center items-center">
@@ -120,25 +156,47 @@ const Frame3 = () => {
             />
           </div>
         </div>
-        <Textarea
-          isReadOnly
-          disableAutosize
-          className=""
-          color="primary"
-          label="CCMan"
-          defaultValue="Whether it's industrial factories, machinery, transportation by land or air, or even shopping malls, all of the mentioned activities are involved in processes that result in the emission of carbon dioxide (CO2)"
-        />
-        <Textarea
-          isReadOnly
-          className="mt-2"
-          color="primary"
-          label="CCMan"
-          defaultValue="When burning fossil fuels like gasoline for vehicles, coal for electricity generation, or wood for cooking, the combustion process releases carbon dioxide (CO2) into the atmosphere
+        <div className="flex">
+          <Button
+            isIconOnly
+            color="primary"
+            aria-label="Audio"
+            className=""
+            onClick={playAudio}
+          >
+            <SpeakerWaveIcon className="h-5 w-5" />
+          </Button>
+          <Textarea
+            isReadOnly
+            disableAutosize
+            className=""
+            color="primary"
+            label="CCMan"
+            defaultValue="Whether it's industrial factories, machinery, transportation by land or air, or even shopping malls, all of the mentioned activities are involved in processes that result in the emission of carbon dioxide (CO2)"
+          />
+        </div>
+        <div className="flex">
+          <Button
+            isIconOnly
+            color="primary"
+            aria-label="Audio"
+            className=""
+            onClick={playAudio}
+          >
+            <SpeakerWaveIcon className="h-5 w-5" />
+          </Button>
+          <Textarea
+            isReadOnly
+            className="mt-2"
+            color="primary"
+            label="CCMan"
+            defaultValue="When burning fossil fuels like gasoline for vehicles, coal for electricity generation, or wood for cooking, the combustion process releases carbon dioxide (CO2) into the atmosphere
           The operation of machinery, such as factory equipment or transportation vehicles, requires energy, and this energy consumption often leads to the burning of fossil fuels within the machinery, resulting in the emission of carbon dioxide (CO2) 
           Air conditioning: The use of air conditioning units in homes or buildings often relies on energy produced from burning fossil fuels, such as oil or natural gas 
           The movement of vehicles, such as cars, airplanes, or ships, typically relies on energy derived from burning fossil fuels, such as gasoline or other combustible fuels. 
           "
-        />
+          />
+        </div>
         {/* <div className="flex flex-wrap gap-4">
           {colors.map((color) => (
             <Popover key={color} placement="bottom" color={color}>
