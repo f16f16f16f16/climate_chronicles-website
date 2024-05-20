f3f88464f975e77e96467538d0e86102432b794f
Nontaphat Pongpis <momo5101@hotmail.com>
2024-05-20 14:26:52 +0700

    Add Questions

---
+++ components/quiz/question.js
@@ -0,0 +1,291 @@
+const questions = [
+  {
+    questionText:
+      "Which greenhouse gas is emitted the most from human activities?",
+    answerOptions: [
+      {
+        answerText: "CO₂",
+        isCorrect: true,
+        explanation:
+          "It is a large amount of greenhouse gas. And it can occur naturally, but CO2 emissions from energy combustion But now humans have become the main contributor to the generation and release of CO2, resulting in an increase in the speed of climate change in general. and is the cause of global warming.",
+      },
+      {
+        answerText: "CH4",
+        isCorrect: false,
+        explanation:
+          "It is the second-largest greenhouse gas released into the earth's atmosphere. It is a gas that exists in nature, but 60 percent of it in the atmosphere is caused by human actions, such as dumping waste in landfills. Livestock farming, animal manure, and the and the burning of fossil fuels in agriculture Wastewater treatment and other industries.",
+      },
+      {
+        answerText: "N2O",
+        isCorrect: false,
+        explanation:
+          "It is another gas that occurs naturally, such as during lightning or volcanic eruptions. Including the decomposition of organic matter. But during the industrial era, humans added about 17 percent of this gas to industries that use nitric acid in their production processes.",
+      },
+      {
+        answerText: "HFCs",
+        isCorrect: false,
+        explanation:
+          "It is a greenhouse gas that is synthesized by humans. It is used in various refrigeration systems and is a substance used to replace chlorofluorocarbon gas (CFCs), which is a substance used in air conditioners, refrigerators, sprays, and fire extinguishers. It has a very high heat storage potential.",
+      },
+    ],
+  },
+  {
+    questionText:
+      "Which of the following activities Add carbon dioxide to the earth's atmosphere?",
+    answerOptions: [
+      {
+        answerText: "Riding a bicycle to go to the market",
+        isCorrect: false,
+        explanation:
+          "Driving a bicycle does not directly release CO2 like driving a car. But it reduces the amount of CO2 gas produced compared to cars.",
+      },
+      {
+        answerText: "Use cloth bags instead of plastic bags.",
+        isCorrect: false,
+        explanation:
+          "Using cloth bags instead of plastic bags reduces the amount of plastic waste that creates CO2, is durable, and can be used many times.",
+      },
+      {
+        answerText: "Increase green space",
+        isCorrect: false,
+        explanation:
+          "increasing green space or planting trees helps in the process of absorbing CO2 from the air in the process of photosynthesis. Reduces the amount of CO2 in the atmosphere.",
+      },
+      {
+        answerText: "Drive a car to go to work.",
+        isCorrect: true,
+        explanation:
+          "Driving a car is an important source of carbon dioxide. By changing the gas engine from the fuel in cars. Using cars as personal vehicles or for transportation causes continuous CO2 emissions. As more cars are driven, the amount of CO2 emitted will also increase.",
+      },
+    ],
+  },
+  {
+    questionText:
+      "How does increasing the amount of CO2 in the air affect the world?",
+    answerOptions: [
+      {
+        answerText: "Increase the global temperature",
+        isCorrect: false,
+        explanation:
+          "CO2 is a gas involved in stimulating a process called the 'greenhouse effect,' in which gases in the atmosphere trap heat from sunlight reflected back from the earth's surface. Increasing the amount of CO2 in the atmosphere will make this process more efficient.",
+      },
+      {
+        answerText: "Changes in the marine ecosystem",
+        isCorrect: false,
+        explanation:
+          "Increasing the amount of CO2 in the atmosphere affects the condition of the marine ecosystem. This may have an impact on the adaptation of animals and plants living in the marine environment. and may also affect the trophic series of organisms in the marine ecosystem.",
+      },
+      {
+        answerText: "Increase the rate of ice melting.",
+        isCorrect: false,
+        explanation:
+          "As more CO2 is added to the atmosphere, air with more CO2 will trap more heat in the earth. This higher temperature can cause the ice to melt faster. This is especially true in areas where the ice is very weak and temperatures are higher.",
+      },
+      {
+        answerText: "All are effects.",
+        isCorrect: true,
+        explanation: "",
+      },
+    ],
+  },
+  {
+    questionText: "Which gas is not a greenhouse gas?",
+    answerOptions: [
+      {
+        answerText: "N2O",
+        isCorrect: false,
+        explanation:
+          "nitrous oxide gas It is a greenhouse gas that has an effect on stimulating global warming.",
+      },
+      {
+        answerText: "CO2",
+        isCorrect: false,
+        explanation:
+          "carbon dioxide It is the most important greenhouse gas and has an impact on stimulating global warming.",
+      },
+      {
+        answerText: "SO2",
+        isCorrect: true,
+        explanation:
+          "SO2 is not a greenhouse gas. Because it has little effect on stimulating global warming. But it has a huge impact on other environments, such as combining with rainwater to become acid rain.",
+      },
+      {
+        answerText: "SF6",
+        isCorrect: false,
+        explanation:
+          "Sulfur hexafluoride It is a greenhouse gas that has the effect of stimulating global warming and has a very high global temperature (GWP).",
+      },
+    ],
+  },
+  {
+    questionText: "Which is not the main cause of global warming?",
+    answerOptions: [
+      {
+        answerText: "Industrial factory",
+        isCorrect: false,
+        explanation:
+          "Industrial factories are the source of greenhouse gas emissions such as CO2, CH4, and N2O, which are the main cause of the increase in the amount of greenhouse gases in the atmosphere.",
+      },
+      {
+        answerText: "Agriculture and animal raising",
+        isCorrect: false,
+        explanation:
+          "The cultivation of crops and animals in the food production cycle is part of the cause of global warming. This is due to the use of chemical fertilizers and straw burning and greenhouse gas emissions from the animal raising process.",
+      },
+      {
+        answerText: "Household",
+        isCorrect: false,
+        explanation:
+          "Household energy use, such as using electricity that comes from energy from unclean fuels, and the use of gas for cooking, resulting in the release of greenhouse gases.",
+      },
+      {
+        answerText: "Use alternative energy.",
+        isCorrect: true,
+        explanation:
+          "The use of renewable energy has become an important way to solve the problem. Renewable energy not only reduces the use of limited natural resources but also reduces greenhouse gas emissions. but also build a sustainable economy and create a more prosperous society in the long run.",
+      },
+    ],
+  },
+  {
+    questionText: "Where does methane gas come from?",
+    answerOptions: [
+      {
+        answerText: "Smoking",
+        isCorrect: false,
+        explanation:
+          "Methane gas is part of cigarette smoke that is created from the combustion of chemicals in cigarettes. But less than other options",
+      },
+      {
+        answerText: "Turn on the fan.",
+        isCorrect: false,
+        explanation:
+          "Turning on the fan creates heat from the motor and CO2, but this is usually a small amount compared to other sources.",
+      },
+      {
+        answerText: "Decomposition of living things",
+        isCorrect: true,
+        explanation:
+          "When living things are decomposed, methane gas is released, such as in the fermentation process. The fermentation of organic materials releases methane gas as a secondary result of the microbial process.",
+      },
+      {
+        answerText: "Use an EV car",
+        isCorrect: false,
+        explanation:
+          "We greatly reduce methane and carbon dioxide emissions caused by the use of gasoline-powered cars. Using EVs allows us to reduce the significant increase in clean energy and reduce our impact on the environment.",
+      },
+    ],
+  },
+  {
+    questionText: "What methods can help reduce the production of GHG?",
+    answerOptions: [
+      {
+        answerText: "Using renewable energy",
+        isCorrect: false,
+        explanation:
+          "Using renewable energy, such as energy derived from solar or wind energy, it will help reduce energy use from sources that emit GHG.",
+      },
+      {
+        answerText: "Use of fossil energy",
+        isCorrect: false,
+        explanation:
+          "The use of energy that comes from fossil sources, including energy from nuclear energy. or energy from hydrogen energy that has a huge effect on the environment.",
+      },
+      {
+        answerText: "Using chemical fertilizers",
+        isCorrect: true,
+        explanation:
+          "The use of chemical fertilizers may have an impact on GHG generation, especially in the production process.",
+      },
+      {
+        answerText: "All answers are correct.",
+        isCorrect: false,
+        explanation: "",
+      },
+    ],
+  },
+  {
+    questionText: "Using an air conditioner will release what GHG gases?",
+    answerOptions: [
+      {
+        answerText: "HFCs",
+        isCorrect: true,
+        explanation:
+          "It is a gas used in air conditioning systems. and refrigeration When released into the atmosphere, it will have the effect of increasing the heat in the atmosphere. and is part of the problem of climate change through the impact of greenhouse gases.",
+      },
+      {
+        answerText: "CH4",
+        isCorrect: false,
+        explanation:
+          "It is another greenhouse gas that has an effect on increasing global temperatures. But it is often caused by natural and human activities, such as the lack of reactions in animals.",
+      },
+      {
+        answerText: "SF6",
+        isCorrect: false,
+        explanation:
+          "It is a greenhouse gas that has an effect on increasing heat in the atmosphere. But it is often used in industrial applications.",
+      },
+      {
+        answerText: "All answers are wrong.",
+        isCorrect: false,
+        explanation: "",
+      },
+    ],
+  },
+  {
+    questionText: "GHG stands for",
+    answerOptions: [
+      {
+        answerText: "Growth Height Green",
+        isCorrect: false,
+        explanation: "",
+      },
+      {
+        answerText: "Greenhouse Gases",
+        isCorrect: true,
+        explanation: "",
+      },
+      {
+        answerText: "Global Health Group",
+        isCorrect: false,
+        explanation: "",
+      },
+      {
+        answerText: "General Housing Grant",
+        isCorrect: false,
+        explanation: "",
+      },
+    ],
+  },
+  {
+    questionText:
+      "How does the area used to plant trees affect the environment?",
+    answerOptions: [
+      {
+        answerText: "Increase the amount of CO2 in the air.",
+        isCorrect: false,
+        explanation:
+          "Planting trees reduces the amount of CO2 in the air by absorbing carbon dioxide to use in the photosynthesis process. It is not an increase in the amount of CO2 in the air.",
+      },
+      {
+        answerText: "Reduce the amount of CO2 in the air.",
+        isCorrect: true,
+        explanation:
+          "Trees have the ability to absorb carbon dioxide (CO2) from the air and use it in the photosynthesis process. Through this process of photosynthesis, trees create food and store carbon dioxide. This carbon dioxide storage reduces the amount of CO2 in the air.",
+      },
+      {
+        answerText: "Increase the amount of dust in the air.",
+        isCorrect: false,
+        explanation:
+          "Trees help trap small dust particles. from the structure of the leaves, which can help reduce small dust particles in the air.",
+      },
+      {
+        answerText: "Reduce the amount of oxygen in the air",
+        isCorrect: false,
+        explanation:
+          "Trees are involved in the process of reducing the amount of oxygen in the air. Oxygen is essential for living things to breathe. And planting trees will not affect the amount of oxygen in the air.",
+      },
+    ],
+  },
+];
+
+export default questions;
